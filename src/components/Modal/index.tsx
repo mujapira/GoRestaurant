6f948b5664ee6263bab14be282c8d3c9b2afc570
Malmas <malmasmello2@gmail.com>
2022-03-15 18:12:46 -0300

    Refactor: Components, Input e Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,28 +1,22 @@
-import { Component } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+interface ModalProps {
+  isOpen: boolean,
+  setIsOpen:() => void,
+  children:ReactNode,
+}
 
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
 
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+export function Modal({ isOpen, setIsOpen, children}: ModalProps) {
+
+  const [modalStatus, setModalStatus] = useState(isOpen)
+
+  useEffect(() => {
+    setModalStatus(isOpen)
+  }, [isOpen])
 
     return (
       <ReactModal
